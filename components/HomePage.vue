5cde99ddf593e9f53553ddb9c0d02c9e093b18f9
parz1 <parz1zhou@outlook.com>
2025-03-14 02:46:46 +0900

    fix: enhance responsive styles in safari

--- components/HomePage.vue
+++ components/HomePage.vue
@@ -33,7 +33,7 @@
           <div class="text-xl mb-0">I build apps mainly with</div>
           <div class="flex flex-wrap justify-start gap-1">
             <div
-              class="h-full flex flex-col items-center p-2 border-2 border-dashed rounded-md dark:bg-gray-800"
+              class="w-[14.5rem] h-full tech-item p-2 border-2 border-dashed rounded-md dark:bg-gray-800"
             >
               <div class="h-[5.5rem] flex gap-2">
                 <TechIcon name="javascript" size="5rem" icon="/svg/javascript-color.svg" />
@@ -53,7 +53,7 @@
             </div>
 
             <div
-              class="h-full flex flex-col items-center p-2 border-2 border-dashed rounded-md dark:bg-gray-800"
+              class="w-[17rem] h-full tech-item p-2 border-2 border-dashed rounded-md dark:bg-gray-800"
             >
               <div class="h-[5.5rem] flex gap-2">
                 <div class="h-full flex flex-col gap-1 flex-wrap">
@@ -75,7 +75,7 @@
               Frameworks
             </div>
             <div
-              class="h-full flex flex-col items-center p-2 border-2 border-dashed rounded-md dark:bg-gray-800"
+              class="w-[10rem] h-full tech-item p-2 border-2 border-dashed rounded-md dark:bg-gray-800"
             >
               <div class="h-[5.5rem] flex gap-2">
                 <div class="h-full flex flex-col gap-1 flex-wrap">
@@ -95,47 +95,6 @@
               Tools
             </div>
           </div>
-
-          <div class="lg:hidden flex gap-1 p-2 border-2 border-dashed rounded-md dark:bg-gray-800">
-            <TechIcon name="javascript" size="4rem" icon="/svg/javascript-color.svg" />
-            <!-- <img class="w-10 h-10" src="/svg/typescript-color.svg" alt="typescript" /> -->
-
-            <TechIcon name="typescript" size="4rem" icon="/svg/typescript-color.svg" />
-            <!-- node -->
-            <TechIcon name="nodejs" size="4rem" icon="/svg/nodedotjs-color.svg" />
-            <TechIcon name="C" size="4rem" icon="/svg/c-color.svg" />
-            <!-- node -->
-            <TechIcon name="C++" size="4rem" icon="/svg/cplusplus-color.svg" />
-            <!-- rust -->
-            <TechIcon name="rust" size="4rem" icon="/svg/rust-color.svg" />
-          </div>
-          <div class="lg:hidden text-lg mt-2">Frameworks&Tools</div>
-          <div
-            class="lg:hidden flex h-[6.8rem] p-2 border-2 border-dashed rounded-md dark:bg-gray-800"
-          >
-            <div class="h-full flex flex-col gap-2 flex-wrap">
-              <TechIcon name="vuejs" size="2.4rem" icon="/svg/vuedotjs-color.svg" />
-              <TechIcon name="nuxtjs" size="2.4rem" icon="/svg/nuxtdotjs-color.svg" />
-              <!-- react -->
-              <TechIcon name="react" size="2.4rem" icon="/svg/react-color.svg" />
-              <TechIcon name="react native" size="2.4rem" icon="/svg/react-native-color.svg" />
-              <TechIcon name="electron" size="2.4rem" icon="/svg/electron-color.svg" />
-              <TechIcon name="nestjs" size="2.4rem" icon="/svg/nestjs.svg" />
-              <TechIcon name="tailwindcss" size="2.4rem" icon="/svg/tailwindcss-color.svg" />
-              <TechIcon name="unity" size="2.4rem" icon="/svg/unity.svg" />
-              <TechIcon name="vite" size="2.4rem" icon="/svg/vite-color.svg" />
-              <TechIcon name="webpack" size="2.4rem" icon="/svg/webpack-color.svg" />
-              <TechIcon name="rollup" size="2.4rem" icon="/svg/rollupdotjs-color.svg" />
-              <TechIcon name="turborepo" size="2.4rem" icon="/svg/turborepo-color.svg" />
-              <div class="h-full px-4 flex flex-col justify-center">...</div>
-            </div>
-
-            <div class="h-full flex flex-col gap-1 flex-wrap ml-4">
-              <!-- vscode -->
-              <TechIcon name="vscode" size="2.4rem" icon="/svg/visualstudiocode-color.svg" />
-              <TechIcon name="arc browser" size="2.6rem" icon="/svg/arc-logo.svg" />
-            </div>
-          </div>
         </div>
       </div>
     </div>
@@ -147,4 +106,15 @@
   </div>
 </template>
 
-<style scope></style>
+<style scope>
+.flex-wrap-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  align-items: center;
+}
+.tech-item {
+  flex-grow: 0; /* 避免 Safari 计算错误 */
+  flex-shrink: 0;
+}
+</style>
