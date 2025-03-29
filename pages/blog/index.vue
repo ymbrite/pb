d3f322762df19440a0a0a513171844fd85d4aeef
parz1 <parz1zhou@outlook.com>
2025-03-30 00:18:38 +0900

    fix: remove unused vars

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -2,17 +2,9 @@
 const { data: articles } = await useAsyncData('latest-posts', () => queryCollection('blog').all(), {
   lazy: true,
 })
-
-const { data: home } = await useAsyncData(async () => {
-  const home = await queryCollection('blog').path('').all()
-  console.log(home)
-  return home
-})
 </script>
 
 <template>
-  <div>{{ home }}</div>
-
   <div class="py-8">
     <ul class="space-y-4">
       <li v-for="(article, index) in articles" :key="index">
